b0eddb7570917d2fc362e215e5304fa9c70ebb02
Camilo Ramirez <Camilo Ramirez>
2016-06-11 16:00:11 +0200

    styled

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -36,7 +36,7 @@ render: function (){
     debugger;
     function renderMessage(){
       if(isLoading){
-        return <h3>Fetching weather...</h3>
+        return <h3 className="text-center">Fetching weather...</h3>
       }else if(temp && location){
         return   <WeatherMessage location={location} temp={temp}/>
       }
@@ -44,7 +44,7 @@ render: function (){
 
     return (
       <div>
-        <h3>Weather component!</h3>
+        <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -5,7 +5,7 @@ var WeatherMessage = (props) =>{
   var temp = props.temp;
   return (
     <div>
-      <h1> The current Temperature in {location} is {temp}</h1>
+      <h1 className="text-center"> The current Temperature in {location} is {temp}</h1>
     </div>
   );
 };
